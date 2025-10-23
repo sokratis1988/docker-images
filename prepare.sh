5b358b69f09e03923f2107a428a48b7bb1cc617d
Sokratis <59183311+sokratis1988@users.noreply.github.com>
2025-10-24 01:22:05 +0200

    update

--- prepare.sh
+++ prepare.sh
@@ -3,15 +3,15 @@
 set -e
 
 function log_info() {
-  >&2 echo -e "[\\e[1;94mINFO\\e[0m] $*"
+  >&2 echo -e "[\e[1;94mINFO\e[0m] $*"
 }
 
 function log_warn() {
-  >&2 echo -e "[\\e[1;93mWARN\\e[0m] $*"
+  >&2 echo -e "[\e[1;93mWARN\e[0m] $*"
 }
 
 function log_error() {
-  >&2 echo -e "[\\e[1;91mERROR\\e[0m] $*"
+  >&2 echo -e "[\e[1;91mERROR\e[0m] $*"
 }
 
 function fail() {
@@ -26,40 +26,50 @@ function assert_argument() {
   fi
 }
 
-
 function get_json_releases() {
+    # Basis: Tag-Namen als Array
     JQ_FILTER='map(.tag_name)'
     JQ_FILTER_LATEST='[.tag_name]'
+
+    # ERC: rc* ausblenden
     if [[ $ERC == true ]]; then
-        #JQ_FILTER='map(select(.tag_name | test("-rc\\.\\d+$") | not))'
-        JQ_FILTER='map(select(.tag_name | test("-(rc|b)[0-9]*\\.?[0-9]*$|rc[0-9]*\\.?[0-9]*$|b[0-9]*\\.?[0-9]*$") | not)) | .[] | .tag_name'
+        JQ_FILTER+=' | map(select(test("(^|[.-])rc[0-9]*([.][0-9]+)*$") | not))'
     fi
+
+    if [[ $EAC == true ]]; then
+        JQ_FILTER+=' | map(select(test("(^|[.-])(alpha|beta)[0-9]*([.][0-9]+)*$") | not))'
+    fi
+
     if [[ $LATEST == true ]]; then
       releases=$(curl ${INSECURE:+-k} -s "$GITHUB_API/repos/$GITHUB_REPO/$GITHUB_PROJECT/releases/latest" | jq -r "$JQ_FILTER_LATEST")
     else
       releases=$(curl ${INSECURE:+-k} -s "$GITHUB_API/repos/$GITHUB_REPO/$GITHUB_PROJECT/releases?per_page=$PER_PAGE" | jq -r "$JQ_FILTER")
     fi
 
-    echo $releases
+    echo "$releases"
 }
 
 function get_from_pypi() {
-  JQ_FILTER='[."releases" | keys[]]'
+  JQ_FILTER='."releases" | keys'
   JQ_FILTER_LATEST='["\( .info.version )"]'
+
   if [[ $ERC == true ]]; then
-      JQ_FILTER='."releases" | keys | map(select(test("^(?!.*(?:rc|a|b)[0-9]+$).*"))) | .[]'
+      JQ_FILTER+=' | map(select(test("(?:[.-]?rc[0-9.]*$)") | not))'
   fi
+
+  if [[ $EAC == true ]]; then
+      JQ_FILTER+=' | map(select(test("(?:[.-]?(?:a|alpha|b|beta)[0-9.]*$)") | not))'
+  fi
+
   if [[ $LATEST == true ]]; then
     releases=$(curl ${INSECURE:+-k} -s "$PYPI_API/$PYPI_PROJECT/json" | jq -r "$JQ_FILTER_LATEST")
   else
     releases=$(curl ${INSECURE:+-k} -s "$PYPI_API/$PYPI_PROJECT/json" | jq -r "$JQ_FILTER")
   fi
 
-  echo $releases
+  echo "$releases"
 }
 
-
-# Init variables
 GITHUB_API=${GITHUB_API:-"https://api.github.com"}
 PYPI_API=${PYPI_API:-"https://pypi.org/pypi"}
 PER_PAGE=100
@@ -77,6 +87,7 @@ case ${key} in
     log_info "  --github-repo (Github Repository)"
     log_info "  --github-project (Github Project)"
     log_info "  [--exclude-release-candidates]"
+    log_info "  [--exclude-alpha]"
     log_info "  [--per-page]"
     log_info "  [--latest]"
     log_info "  [--insecure]"
@@ -115,8 +126,13 @@ case ${key} in
     ERC="true"
     shift
     ;;
+    --exclude-alpha)
+    EAC="true"
+    shift
+    ;;
     --per-page)
     PER_PAGE="$2"
+    shift
     shift
     ;;
     --latest)
@@ -140,17 +156,18 @@ fi
 
 log_info "Get releases"
 if [[ $PYPI_PROJECT == "" ]]; then
-log_info "- github api           (--github-api)                   : \\e[33;1m${GITHUB_API}\\e[0m"
-log_info "- github repository    (--github-repo)                  : \\e[33;1m${GITHUB_REPO}\\e[0m"
-log_info "- github project       (--github-project)               : \\e[33;1m${GITHUB_PROJECT}\\e[0m"
+log_info "- github api           (--github-api)                   : \e[33;1m${GITHUB_API}\e[0m"
+log_info "- github repository    (--github-repo)                  : \e[33;1m${GITHUB_REPO}\e[0m"
+log_info "- github project       (--github-project)               : \e[33;1m${GITHUB_PROJECT}\e[0m"
 else
-log_info "- pypi api             (--pypi-api)                     : \\e[33;1m${PYPI_API}\\e[0m"
-log_info "- pypi project         (--pypi-project)                 : \\e[33;1m${PYPI_PROJECT}\\e[0m"
+log_info "- pypi api             (--pypi-api)                     : \e[33;1m${PYPI_API}\e[0m"
+log_info "- pypi project         (--pypi-project)                 : \e[33;1m${PYPI_PROJECT}\e[0m"
 fi
-log_info "- release candidates   (--exclude-release-candidates)   : \\e[33;1m${ERC:-false}\\e[0m"
-log_info "- latest               (--latest)                       : \\e[33;1m${LATEST:-false}\\e[0m"
-log_info "- per page             (--per-page)                     : \\e[33;1m${PER_PAGE}\\e[0m"
-log_info "- insecure             (--insecure)                     : \\e[33;1m${INSECURE:-false}\\e[0m"
+log_info "- release candidates   (--exclude-release-candidates)   : \e[33;1m${ERC:-false}\e[0m"
+log_info "- alpha/beta           (--exclude-alpha)                : \e[33;1m${EAC:-false}\e[0m"
+log_info "- latest               (--latest)                       : \e[33;1m${LATEST:-false}\e[0m"
+log_info "- per page             (--per-page)                     : \e[33;1m${PER_PAGE}\e[0m"
+log_info "- insecure             (--insecure)                     : \e[33;1m${INSECURE:-false}\e[0m"
 
 if [[ $PYPI_PROJECT == "" ]]; then 
 get_json_releases
